7bef70fad9c8cd991004395d4d8b7e02c1c9736e
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-03-20 00:06:18 -0400

    Added resources section and updated header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import Image from 'next/image';
 import Layout from '@/components/Layout';
+import HorizontalLine from '@/widgets/HorizontalLine';
+import Resources from '@/components/HomePage/Resources';
 
 export default function Home() {
   return (
     <Layout>
-      <div className="md:flex">
-        <div>
-          <h1 className="text-4xl font-display">Anthony Zhou</h1>
+      <div className="md:flex space-x-10 justify-between">
+        <div className="flex flex-col justify-start">
+          <h1 className="text-5xl font-display mb-10 mt-10">Anthony Zhou</h1>
           <p>Psychology, Economics, Programming, or anything interesting.</p>
         </div>
-        <div>
-          Image goes here.
-        </div>
+        <Image className="rounded-3xl" src="/images/claymation_piano.png" alt="Claymation piano with bookshelf in background." width={260} height={260} />
       </div>
+      <HorizontalLine />
+      <div>
+        <h2 className="text-2xl font-display text-center">Latest</h2>
+        <p>Latest blog posts go here.</p>
+      </div>
+      <div className="mt-6">
+        <h2 className="text-2xl font-display text-center">My Favorites</h2>
+        <p>Favorite blog posts go here.</p>
+      </div>
+      <HorizontalLine />
+      <Resources />
+      <HorizontalLine />
     </Layout>
   );
 }
