61447b43b10b89630f70734c177acf8ab6617132
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-05-09 10:04:36 -0500

    Add books page

---
+++ src/pages/books.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '@/components/Layout';
+
+export default function Books() {
+  return (
+    <Layout>
+      <div className="text-[#4B4B4B]">
+        <div className="mb-5 mt-5">I study computer science and cognitive science at Columbia University in New York.</div>
+      </div>
+    </Layout>
+  );
+}
