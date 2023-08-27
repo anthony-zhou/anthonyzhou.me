62e6fd732fc5c6c3eeb37c58d8692ce2c9cc4868
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-08-27 17:44:26 -0400

    Add subscribe form

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
   return (
     <div className="flex justify-between pb-10">
       <div className="text-sm">
-        Contact:
+        {/* Contact:
         {' '}
-        <a href="mailto:anthony.zhou03@gmail.com">anthony.zhou03@gmail.com</a>
+        <a href="mailto:anthony.zhou03@gmail.com">anthony.zhou03@gmail.com</a> */}
       </div>
       <div className="">
         {/* Social media links */}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Header';
+import SubscribeForm from '../SubscribeForm';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,6 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="container mx-auto pb-10 px-10">
         {children}
       </div>
+      <SubscribeForm />
     </div>
   );
 }
