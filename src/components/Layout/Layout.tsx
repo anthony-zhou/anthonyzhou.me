7fc4ccc5566b0d643c32485644ed7848a9d6b3ff
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-08-26 21:41:35 -0400

    Add bottom padding in layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
-      <div className="container mx-auto">
+      <div className="container mx-auto pb-10">
         {children}
       </div>
     </div>
