efca3d0f5e41d095c30eec70c90099659de94b04
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-08-27 11:00:28 -0400

    Fix lintting issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home({ allPostsData, featuredPostsData }: HomeProps) {
 
   const movePage = (newPageNum: number) => {
     setPageNumber(newPageNum);
-    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' } as unknown as ScrollToOptions); // see https://github.com/microsoft/TypeScript/issues/47441
   };
 
   return (
