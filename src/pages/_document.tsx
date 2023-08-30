3e07f3eacee3e93bf789f5222ce955796cf7e344
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-08-30 14:06:38 -0400

    Add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import {
-  Html, Head, Main, NextScript,
+  Html, Main, NextScript,
 } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=UA-155734640-1" />
+      <Script id="google-analytics">
+        {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+  
+           gtag('config', 'UA-155734640-1');
+         `}
+      </Script>
       <body>
         <Main />
         <NextScript />
